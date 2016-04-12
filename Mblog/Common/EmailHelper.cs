ce1e03868676534079be0c0ba85e61f143285e64
everglow125 <183900112@qq.com>
2016-04-12 15:09:03 +0800

    添加发送邮件模块

---
+++ Mblog/Common/EmailHelper.cs
@@ -0,0 +1,76 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class EmailHelper
+    {
+        public static void SendEmail(EmailInfo mail)
+        {
+            MailAddress from = new MailAddress(mail.SenderMail, mail.NickName);
+            MailAddress to = new MailAddress(mail.RecipientMail);
+            using (MailMessage mailmessage = new MailMessage(from, to))
+            {
+                mailmessage.Subject = mail.Title;
+                mailmessage.Body = mail.Content;
+                mailmessage.BodyEncoding = System.Text.Encoding.Default;
+                mailmessage.IsBodyHtml = true;
+                mailmessage.Priority = MailPriority.Normal;
+                mailmessage.DeliveryNotificationOptions = DeliveryNotificationOptions.OnSuccess;
+                using (SmtpClient client = new SmtpClient(mail.StmpServer))
+                {
+                    client.UseDefaultCredentials = false;
+                    client.EnableSsl = mail.EnableSSL;
+                    client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                    client.Credentials = new System.Net.NetworkCredential(mail.MailAccount, mail.MailPassword);
+                    client.Send(mailmessage);
+                }
+            }
+        }
+    }
+
+    public class EmailInfo
+    {
+        /// <summary>
+        /// 发件人邮箱
+        /// </summary>
+        public string SenderMail { get; set; }
+        /// <summary>
+        /// 发件人署名
+        /// </summary>
+        public string NickName { get; set; }
+        /// <summary>
+        /// 收件人邮箱
+        /// </summary>
+        public string RecipientMail { get; set; }
+        /// <summary>
+        /// 标题
+        /// </summary>
+        public string Title { get; set; }
+        /// <summary>
+        /// 内容
+        /// </summary>
+        public string Content { get; set; }
+        /// <summary>
+        /// STMP服务器 如smtp.163.com
+        /// </summary>
+        public string StmpServer { get; set; }
+        /// <summary>
+        /// 发件人邮箱账号 @前面部分
+        /// </summary>
+        public string MailAccount { get; set; }
+        /// <summary>
+        /// 发件人邮箱密码  明文
+        /// </summary>
+        public string MailPassword { get; set; }
+        /// <summary>
+        /// 是否需要SSL加密 
+        /// </summary>
+        public bool EnableSSL { get; set; }
+    }
+}
---
+++ Mblog/CommonTests/EmailHelperTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace Common.Tests
+{
+    [TestClass()]
+    public class EmailHelperTests
+    {
+        [TestMethod()]
+        public void SendEmailTest()
+        {
+            EmailInfo mail = new EmailInfo()
+            {
+                Content = "测试邮件发送",
+                MailAccount = "liaojin_work",
+                MailPassword = "********",
+                EnableSSL = false,
+                NickName = "测试账号",
+                RecipientMail = "183900112@qq.com",
+                SenderMail = "liaojin_work@163.com",
+                StmpServer = "smtp.163.com",
+                Title = "测试邮件请勿回复"
+
+            };
+            EmailHelper.SendEmail(mail);
+            Assert.Fail();
+        }
+    }
+}
