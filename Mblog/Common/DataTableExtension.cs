df9a23d7474799d42f5641859c883a5b347ab350
everglow125 <183900112@qq.com>
2016-04-12 14:22:50 +0800

    新增行转列方法

---
+++ Mblog/Common/DataTableExtension.cs
@@ -0,0 +1,92 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public static class DataTableExtension
+    {
+        /// <summary>
+        /// 关系表转 行列对应表
+        /// </summary>
+        /// <param name="dt"></param>
+        /// <param name="toColumn">该列值转成列名</param>
+        /// <param name="toRow">该列名转成行值</param>
+        /// <param name="value">该列为行列对应值</param>
+        /// <returns></returns>
+        public static DataTable ReversalRows(this DataTable dt, string toColumn, string toRow, string value)
+        {
+            DataTable result = new DataTable();
+            result.Columns.Add(toRow);
+            List<string> rows = new List<string>();
+            List<string> columns = new List<string>();
+            Dictionary<string, int> cells = new Dictionary<string, int>();
+            int rowIndex = 0;
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                var columnName = dt.Rows[i][toColumn].ToString();
+                if (!result.Columns.Contains(columnName))
+                {
+                    result.Columns.Add(columnName);
+                    columns.Add(columnName);
+                }
+
+                var rowValue = dt.Rows[i][toRow].ToString();
+                if (!rows.Contains(rowValue))
+                {
+                    rows.Add(rowValue);
+                    cells.Add(rowValue, rowIndex++);
+                }
+            }
+            foreach (var item in rows)
+            {
+                DataRow dr = result.NewRow();
+                dr[toRow] = item;
+                result.Rows.Add(dr);
+            }
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                var rowNum = cells[dr[toRow].ToString()];
+                var columnName = dr[toColumn].ToString();
+                result.Rows[rowNum][columnName] = dr[value].ToString();
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// 将第一列转为行
+        /// </summary>
+        /// <param name="dt"></param>
+        /// <param name="fixedColumnName">列名转成行以后所属的列名</param>
+        /// <returns></returns>
+        public static DataTable ReversalRows(this DataTable dt, string fixedColumnName)
+        {
+            DataTable result = new DataTable();
+            result.Columns.Add(fixedColumnName);
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                result.Columns.Add(dt.Rows[i][0].ToString());
+            }
+            for (int i = 1; i < dt.Columns.Count; i++)
+            {
+                DataRow dr = result.NewRow();
+                dr[0] = dt.Columns[i].ColumnName;
+                result.Rows.Add(dr);
+            }
+            for (int i = 1; i <= dt.Rows.Count; i++)
+            {
+                for (int j = 1; j < dt.Columns.Count; j++)
+                {
+                    result.Rows[j - 1][i] = dt.Rows[i - 1][j];
+                }
+            }
+            return result;
+        }
+    }
+
+}
---
+++ Mblog/CommonTests/DataTableExtensionTests.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data;
+
+namespace Common.Tests
+{
+    [TestClass()]
+    public class DataTableExtensionTests
+    {
+        [TestMethod()]
+        public void ReversalRowsTest()
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("姓名");
+            dt.Columns.Add("学科");
+            dt.Columns.Add("成绩");
+
+            DataRow dr = dt.NewRow();
+            dr["姓名"] = "张三";
+            dr["学科"] = "语文";
+            dr["成绩"] = 90;
+            dt.Rows.Add(dr);
+
+            DataRow dr1 = dt.NewRow();
+            dr1["姓名"] = "张三";
+            dr1["学科"] = "数学";
+            dr1["成绩"] = 70;
+            dt.Rows.Add(dr1);
+
+            DataRow dr2 = dt.NewRow();
+            dr2["姓名"] = "李四";
+            dr2["学科"] = "英语";
+            dr2["成绩"] = 90;
+            dt.Rows.Add(dr2);
+
+            DataRow dr3 = dt.NewRow();
+            dr3["姓名"] = "王五";
+            dr3["学科"] = "语文";
+            dr3["成绩"] = 90;
+            dt.Rows.Add(dr3);
+
+            var result = dt.ReversalRows("姓名", "学科", "成绩");
+
+            Assert.AreNotEqual(dt, result);
+        }
+
+        [TestMethod()]
+        public void ReversalRowsTest1()
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("姓名");
+            dt.Columns.Add("数学");
+            dt.Columns.Add("语文");
+            dt.Columns.Add("英语");
+
+            DataRow dr = dt.NewRow();
+            dr["姓名"] = "乔峰";
+            dr["数学"] = 99;
+            dr["语文"] = 99;
+            dr["英语"] = 99;
+            dt.Rows.Add(dr);
+
+            DataRow dr1 = dt.NewRow();
+            dr1["姓名"] = "段誉";
+            dr1["数学"] = 90;
+            dr1["语文"] = 80;
+            dr1["英语"] = 80;
+            dt.Rows.Add(dr1);
+
+            DataRow dr2 = dt.NewRow();
+            dr2["姓名"] = "虚竹";
+            dr2["数学"] = 70;
+            dr2["语文"] = 90;
+            dr2["英语"] = 60;
+            dt.Rows.Add(dr2);
+
+            DataRow dr3 = dt.NewRow();
+            dr3["姓名"] = "慕容";
+            dr3["数学"] = 95;
+            dr3["语文"] = 60;
+            dr3["英语"] = 70;
+            dt.Rows.Add(dr3);
+
+            DataRow dr4 = dt.NewRow();
+            dr4["姓名"] = "李寻欢";
+            dr4["数学"] = 100;
+            dr4["语文"] = 100;
+            dr4["英语"] = 100;
+            dt.Rows.Add(dr4);
+
+            var result = dt.ReversalRows("学科");
+
+            Assert.AreNotEqual(dt, result);
+        }
+    }
+}
