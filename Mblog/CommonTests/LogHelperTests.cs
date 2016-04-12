24bc427863ac23449651aaf54657d05cf5834f9b
everglow125 <183900112@qq.com>
2016-04-12 17:15:35 +0800

    添加日志

---
+++ Mblog/CommonTests/LogHelperTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace Common.Tests
+{
+    [TestClass()]
+    public class LogHelperTests
+    {
+        [TestMethod()]
+        public void ErrorLogTest()
+        {
+            try { int a = Convert.ToInt32("aaa"); }
+            catch (Exception ex)
+            {
+                LogHelper.ErrorLog("test", ex);
+            }
+            Assert.Fail();
+        }
+    }
+}
